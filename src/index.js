b7dfa25472243f9bafb8bae6a7c6b9da7fde9586
Erlend Hamberg <eh@blankoslo.no>
2016-07-04 17:53:55 +0200

    Replace axios with whatwg-fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require( './styles/main.less' );
-var axios = require('axios');
+require('whatwg-fetch');
 
 var Elm = require( './Main' );
 var app = Elm.Main.embed(document.getElementById('app'), {
@@ -12,24 +12,25 @@ app.ports.fetchFile.subscribe(function(args) {
   jwt = args[1];
   filename = args[2];
 
-  axios.request({
-    url: url,
-    method: 'get',
-    responseType: 'blob',
-    headers: {
-      'Accept': 'text/csv',
-      'Authorization': 'Bearer ' + jwt
-    }
-  })
-  .then(function(response) {
-    // create a blob url representing the data
-    var url = window.URL.createObjectURL(response.data);
+fetch(url, {
+  method: 'GET',
+  headers: {
+    'Authorization': 'Bearer ' + jwt
+  }
+})
+.then(function(response) {
+  return response.arrayBuffer();
+})
+.then(function(data) {
+  // create a blob url representing the data
+  var blob = new Blob([data]);
+  var url = window.URL.createObjectURL(blob);
 
-    // attach blob url to anchor element with download attribute
-    var anchor = document.createElement('a');
-    anchor.setAttribute('href', url);
-    anchor.setAttribute('download', filename);
-    anchor.click();
-    window.URL.revokeObjectURL(url);
-  });
+  // attach blob url to anchor element with download attribute
+  var anchor = document.createElement('a');
+  anchor.setAttribute('href', url);
+  anchor.setAttribute('download', filename);
+  anchor.click();
+  window.URL.revokeObjectURL(url);
+});
 });
