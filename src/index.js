54686e697d92d2b0542d6d083f42c335287ebae0
Erlend Hamberg <eh@blankoslo.no>
2016-07-04 14:22:44 +0200

    Authenticate and launch file downloads from javascript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 require( './styles/main.less' );
+var axios = require('axios');
 
 var Elm = require( './Main' );
 var app = Elm.Main.embed(document.getElementById('app'), {
   token: window.apiToken,
   apiUrl: window.config.apiUri
+});
+
+app.ports.fetchFile.subscribe(function(args) {
+  url = args[0];
+  jwt = args[1];
+  filename = args[2];
+
+  console.log('args', arguments);
+
+  axios.request({
+    url: url,
+    method: 'get',
+    headers: {
+      'Accept': 'text/csv',
+      'Authorization': 'Bearer ' + jwt
+    }
+  })
+  .then(function(response) {
+    var data = response.data;
+
+    // transform the response into a file
+    var blob = new Blob([data], { type: response.headers['content-type'] });
+    var url = window.URL.createObjectURL(blob);
+
+    // attach blob url to anchor element with download attribute
+    var anchor = document.createElement('a');
+    anchor.setAttribute('href', url);
+    anchor.setAttribute('download', filename);
+    anchor.click();
+    window.URL.revokeObjectURL(url);
+  });
 });
