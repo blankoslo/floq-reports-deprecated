767ca56e12bec53564fa0f000ebb8c10893f4dd3
Sigurd Grøneng <groneng93@gmail.com>
2018-10-25 10:22:16 +0200

    Add methods for grouping time entries on dates and projects before creating csv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require( './styles/main.less' );
 require('whatwg-fetch');
+const csvExporter = require('export-to-csv')
 
 var Elm = require( './Main' );
 var app = Elm.Main.embed(document.getElementById('app'), {
@@ -7,25 +8,20 @@ var app = Elm.Main.embed(document.getElementById('app'), {
   apiUrl: window.config.apiUri
 });
 
+const getFetchConfig = ( payload, accept = 'text/csv' ) => ({
+  method: payload ? 'POST' : 'GET',
+  headers: {
+    'Authorization': 'Bearer ' + window.apiToken,
+    'Content-Type': 'application/json',
+    'Accept': accept,
+  },
+  body: payload ? JSON.stringify(payload) : undefined,
+})
+
 app.ports.fetchFile.subscribe(function(args) {
-  url = args[0];
-  jwt = args[1];
-  filename = args[2];
-  let payload = args[3];
-  payload = payload ? {
-    employee_id: parseInt(payload.split(',')[0]),
-    from_date: payload.split(',')[1],
-    to_date: payload.split(',')[2],
-  } : undefined
-  fetch(url, {
-    method: payload ? 'POST' : 'GET',
-    headers: {
-      'Authorization': 'Bearer ' + jwt,
-      'Content-Type': 'application/json',
-      'Accept': 'text/csv',
-    },
-    body: payload ? JSON.stringify(payload) : undefined,
-  })
+  let [url, jwt, filename] = args
+  
+  fetch(url, getFetchConfig())
     .then(function(response) {
       if (!response.ok) {
         return response.json()
@@ -50,3 +46,74 @@ app.ports.fetchFile.subscribe(function(args) {
     })
     .catch( err => console.log(err) )
 });
+
+const createPayloadFromString = ( payload ) => {
+  return payload = payload
+  ? {
+      employee_id: parseInt( payload.split(',')[0] ),
+      from_date: payload.split( ',' )[1],
+      to_date: payload.split( ',' )[2],
+    } 
+  : undefined 
+}
+
+const fetchEmployeeHours = ( payload ) => {
+  return fetch( `${window.config.apiUri}/rpc/entries_sums_for_employee_with_project`, getFetchConfig(createPayloadFromString(payload), 'application/json') )
+    .then( res => res.json() )
+    .then( groupDataOnDates )
+    .catch( err => console.log(err) )
+}
+
+const getAllDates = ( data ) => {
+  return Array.from( new Set(data.map(element => element.work_date)) )
+    .sort( (a, b) => new Date(a).getTime() - new Date(b).getTime() )
+}
+
+const emptyArrayOfLength = ( projects ) => projects.map( e => 0 )
+
+const createProjectColumn = ( project, hours, dates ) => {
+  const projectRow = { project }
+  dates.forEach( (work_date, index) => { projectRow[work_date] = hours[index] } )
+  return projectRow
+}
+
+const groupDataOnDates = ( timeEntries ) => {
+  const dates = getAllDates( timeEntries )
+  const hoursGroupedOnProjects = {}
+  timeEntries
+    .filter( entry => entry.project ) /* Dates with no hours will have project 'null' */
+    .forEach( ({ hours, work_date, project }) => {
+      const row = hoursGroupedOnProjects[project] || emptyArrayOfLength( dates )
+      row[dates.indexOf( work_date )] = hours
+      hoursGroupedOnProjects[project] = row
+    })
+  timeEntries.forEach( ({ hours, work_date }) => {
+    const row = hoursGroupedOnProjects.total || emptyArrayOfLength( dates )
+    row[dates.indexOf( work_date )] +=  hours
+    hoursGroupedOnProjects.total = row
+  })
+  return Object.keys( hoursGroupedOnProjects )
+    .map( project => createProjectColumn(project, hoursGroupedOnProjects[project], dates) )
+}
+
+const convertToCsv = ( jsonData, filename ) => {
+  const options = {
+    filename,
+    fieldSeparator: ',',
+    quoteStrings: '"',
+    decimalseparator: '.',
+    showLabels: true,
+    showTitle: true,
+    title: filename,
+    useBom: true,
+    useKeysAsHeaders: true,
+  };
+  const csvConverter = new csvExporter.ExportToCsv( options )
+  return csvConverter.generateCsv( jsonData )
+}
+
+app.ports.fetchEmployeeHoursFile.subscribe( function(args) {
+  const [filename, payload] = args
+  fetchEmployeeHours(payload)
+    .then( jsonData => convertToCsv(jsonData, filename) )
+})
